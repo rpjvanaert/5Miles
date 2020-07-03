7dfe249912db0b3bb71f8f9592772f0f8c0c88ae
Logophilist <ralf@van-aert.nl>
2020-07-03 20:32:12 +0100

    File download update

--- failurism/app/src/main/java/com/example/failurism/Activities/DetailActivity.java
+++ failurism/app/src/main/java/com/example/failurism/Activities/DetailActivity.java
@@ -3,16 +3,24 @@ package com.example.failurism.Activities;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.PersistableBundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.Request;
+import com.bumptech.glide.request.target.SizeReadyCallback;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
 import com.example.failurism.ApiManagement.ApiImage;
 import com.example.failurism.R;
 import com.squareup.picasso.Picasso;
@@ -58,16 +66,20 @@ public class DetailActivity extends AppCompatActivity {
         String fileName = "ImageNumber_" + sdf.format(new Date());
         String file = detailImage.getUrl();
         String fileExtension = file.substring(file.length() - 4);
-        File dir = getFilesDir();
         Log.d(DetailActivity.class.getName(), "File link: " + file);
 
         try {
             InputStream in = new URL(file).openStream();
-            FileOutputStream fos = getApplicationContext().openFileOutput("fiveMiles_" + fileName + fileExtension, Context.MODE_PRIVATE);
+            FileOutputStream fos = getApplicationContext().openFileOutput(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + R.string.app_name + "/" + "fiveMiles_" + fileName + fileExtension, Context.MODE_PRIVATE);
             ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            byte[] target = new byte[in.available()];
-            stream.write(target);
+            byte[] data = new byte[file.length()];
+            int nRead;
+            while ((nRead = in.read(data, 0, data.length)) != -1){
+                stream.write(data, 0, nRead);
+            }
             fos.write(stream.toByteArray());
+            in.close();
+            fos.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (MalformedURLException e) {
@@ -75,5 +87,7 @@ public class DetailActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+//        Glide.with(getBaseContext()).load(detailImage.getUrl())
     }
 }
